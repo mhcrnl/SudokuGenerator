5b5c6ff87c146801be3dd269d64c267baacacd14
mhcrnl <mhcrnl@gmail.com>
2018-01-21 10:14:53 +0200

    21/01/2018

---
+++ MhcrnlSudoku/controller.cpp
@@ -0,0 +1,18 @@
+/**
+	Author:		Mihai Cornel	Romania			mhcrnl@gmail.com
+	System:		Ubuntu 16.04	Code::Blocks 13.12	gcc 5.4.0
+                Fedora 23	    Code::Blocks 16.01	gcc 5.3.1
+	Copyright:	@2018
+	Problem:    Controller for Sudoku.
+*/
+#include <iostream>
+#include "controller.h"
+#include "Board.h"
+#include "Cell.h"
+
+using namespace std;
+
+void controller (void){
+
+
+}
---
+++ MhcrnlSudoku/controller.h
@@ -0,0 +1,6 @@
+#ifndef CONTROLLER_H_INCLUDED
+#define CONTROLLER_H_INCLUDED
+
+void controller(void);
+
+#endif // CONTROLLER_H_INCLUDED
